09639de4cebf0beb64368a6ff84c010266f97bd9
yingkailiang <yingkailiang0920@gmail.com>
2013-07-12 11:50:25 +0800

    add linux-dev

---
+++ linux-dev/version.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+#Dual/Quad Core arms are now more prevalent, so don't just limit to x86:
+CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+unset GIT_OPTS
+unset GIT_NOEDIT
+LC_ALL=C git help pull | grep -m 1 -e "--no-edit" >/dev/null 2>&1 && GIT_NOEDIT=1
+
+if [ "${GIT_NOEDIT}" ] ; then
+	GIT_OPTS="${GIT_OPTS} --no-edit"
+fi
+
+config="omap2plus_defconfig"
+
+#linaro_toolchain="arm9_gcc_4_7"
+#linaro_toolchain="cortex_gcc_4_6"
+linaro_toolchain="cortex_gcc_4_7"
+#linaro_toolchain="cortex_gcc_4_8"
+
+#Kernel/Build
+KERNEL_REL=3.8
+KERNEL_TAG=${KERNEL_REL}.13
+BUILD=bone21.2
+
+#v3.X-rcX + upto SHA
+#KERNEL_SHA=""
+
+#git branch
+BRANCH="am33x-v3.8"
+
+BUILDREV=1.0
+DISTRO=cross
+DEBARCH=armhf
